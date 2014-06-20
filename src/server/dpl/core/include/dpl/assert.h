tizenorg/platform.core.security.security-server

DPL: udate dpl/core/include/dpl/assert.h from latest wrt-commons
Needed to get AssertMsg macro, used in other DPL modules.

Change-Id: Iaca6c09a52e2f6644580d667a6817cfaa777d0a4
Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

src/server/dpl/core/include/dpl/assert.h
@@ -40,4 +40,13 @@ SECURITYSERVER_NORETURN void AssertProc(const char *condition,
                                                                    __FUNCTION__); \
                                } } while (0)
 
+#define AssertMsg(Condition, Msg)                                         \
+    do {                                                                  \
+        if (!(Condition)) {                                               \
+            SecurityServer::AssertProc(                                              \
+                (std::string(std::string(#Condition)+" ") + Msg).c_str(), \
+                __FILE__, __LINE__, __FUNCTION__);                        \
+        }                                                                 \
+    } while (0)
+
 #endif // SECURITY_SERVER_ASSERT_H

tizenorg/platform.core.security.security-manager

src/server/dpl/core/include/dpl/assert.h
@@ -40,4 +40,13 @@ SECURITYMANAGER_NORETURN void AssertProc(const char *condition,
                                                                    __FUNCTION__); \
                                } } while (0)
 
+#define AssertMsg(Condition, Msg)                                         \
+    do {                                                                  \
+        if (!(Condition)) {                                               \
+            SecurityManager::AssertProc(                                              \
+                (std::string(std::string(#Condition)+" ") + Msg).c_str(), \
+                __FILE__, __LINE__, __FUNCTION__);                        \
+        }                                                                 \
+    } while (0)
+
 #endif // SECURITY_MANAGER_ASSERT_H