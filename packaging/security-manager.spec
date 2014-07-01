tizenorg/platform.core.security.security-manager

commit 3e62e851a993c620c1167fb2831ecdcd17619ae8

spec: fix %post, %postun and %preun sections
Use standard systemd macros, add missing calls to ldconfig.

Change-Id: Id9fdd874f725e1793f155d2766b8a25fee2df4db
Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

packaging/security-manager.spec
@@ -82,30 +82,17 @@ ln -s ../security-manager-installer.socket %{buildroot}/%{_unitdir}/sockets.targ
 rm -rf %{buildroot}
 
 %post
-systemctl daemon-reload
-if [ $1 = 1 ]; then
-    # installation
-    systemctl start security-manager.service
-fi
-
-if [ $1 = 2 ]; then
-    # update
-    systemctl restart security-manager.service
-fi
+/sbin/ldconfig
+%systemd_post security-manager.service
 chsmack -a System %{TZ_SYS_DB}/.security-manager.db
 chsmack -a System %{TZ_SYS_DB}/.security-manager.db-journal
 
 %preun
-if [ $1 = 0 ]; then
-    # unistall
-    systemctl stop security-manager.service
-fi
+%systemd_preun security-manager.service
 
 %postun
-if [ $1 = 0 ]; then
-    # unistall
-    systemctl daemon-reload
-fi
+/sbin/ldconfig
+%systemd_postun_with_restart security-manager.service
 
 %post -n libsecurity-manager-client -p /sbin/ldconfig
 

commit 7cbe7ccf99f2f58ab836328aeeb9cdf63687ac37

Fix buld break on x86_64, regression in 3e62e851
Unify systemd installation directories between spec and CMake, avoiding
problems on x86_64 (/usr/lib64 vs. /usr/lib).

Change-Id: I5db9cf50978f20d318f7d11349d5437b184b394f
Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

packaging/security-manager.spec
@@ -60,6 +60,7 @@ export LDFLAGS+="-Wl,--rpath=%{_libdir}"
 %cmake . -DVERSION=%{version} \
         -DBIN_INSTALL_DIR=%{_bindir} \
         -DDB_INSTALL_DIR=%{TZ_SYS_DB} \
+        -DSYSTEMD_INSTALL_DIR=%{_unitdir} \
         -DCMAKE_BUILD_TYPE=%{?build_type:%build_type}%{!?build_type:RELEASE} \
         -DCMAKE_VERBOSE_MAKEFILE=ON
 make %{?jobs:-j%jobs}

systemd/CMakeLists.txt
@@ -5,5 +5,5 @@ INSTALL(FILES
     security-manager.target
     security-manager-installer.socket
     DESTINATION
-    ${LIB_INSTALL_DIR}/systemd/system
+    ${SYSTEMD_INSTALL_DIR}/systemd/system
 )