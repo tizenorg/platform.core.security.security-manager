19fa648064ba9aa6f58ce590cb4800c959cbed95
Sebastian Grabowski <s.grabowski@samsung.com>
2014-11-05 12:40:46 +0100

    Move common functions for applications installations
    There are some functions that would be nice to have in a common library
    for the upcoming offline mode. For this, service-common.cpp file has
    been added. The new function AppInstall is now reposible for proper
    applications installations.
    
    Change-Id: I5a807b5ada91843e26b48310807581e47990ef02
    Signed-off-by: Sebastian Grabowski <s.grabowski@samsung.com>

---
+++ src/common/include/service-common.h
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Contact: Rafal Krypa <r.krypa@samsung.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        service-common.h
+ * @author      Sebastian Grabowski (s.grabowski@samsung.com)
+ * @version     1.0
+ * @brief       Implementation of common routines for a service
+ */
+/* vim: set ts=4 et sw=4 tw=78 : */
+
+#pragma once
+
+#include <sys/types.h>
+
+#include <protocols.h>
+#include <privilege_db.h>
+
+namespace SecurityManager {
+
+bool isGlobalUser(uid_t uid);
+
+/*
+ * This function is used to install applications.
+ *
+ * \param[in] Reference to a db object
+ * \param[in] Reference to app_inst_req structure
+ * \return API return code or error code
+ */
+int AppInstall(PrivilegeDb &pdb, const app_inst_req &req);
+
+} // namespace SecurityManager
+
