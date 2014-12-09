bb8b396e7aa364f416317a31a88d1d6c795da25d
Marcin Lis <m.lis@samsung.com>
2014-12-09 13:27:00 +0100

    Add security-manager policy structures and API stub
    Change-Id: Ie5cffe5cefc9e8aea8040f7978517b331809fea2
    Signed-off-by: Marcin Lis <m.lis@samsung.com>

--- src/common/include/protocols.h
+++ src/common/include/protocols.h
@@ -128,6 +128,25 @@ enum class SecurityModuleCall
     USER_DELETE,
 };
 
+struct PolicyUpdateUnit {
+    std::string userId;    // uid converted to string
+    std::string appId;     // application identifier
+    std::string privilege; // Cynara privilege
+    int userType;          // user type - mapped from gumd
+    int value;             // policy to be set, corresponds to Cynara's policy result type
+};
+typedef struct PolicyUpdateUnit PolicyUpdateUnit;
+
+/*struct PolicyEntry {
+    std::string name; // name of entry: application or Cynara privilege
+    int maxValue;     // holds the maximum policy status type allowed to be set for this entry
+    int current;      // holds the current policy status for this entry
+};*/
+
 } // namespace SecurityManager
+
+struct policy_update_req {
+    std::vector<SecurityManager::PolicyUpdateUnit> units;
+};
 
 #endif // _SECURITY_MANAGER_PROTOCOLS_
