17b86a020ef167c05a2c67cd362e9e014f1be7c3
Sebastian Grabowski <s.grabowski@samsung.com>
2014-12-15 11:18:34 +0100

    Added security-manager-unit-tests rpm package.
    This new package is just for having unit tests in order to improve
    security-manager verification process.
    Moreover, first test was added: testing buckets management by
    CynaraAdmin class.
    
    Change-Id: I1d36141a402dbd89a80cc4c0686500352aac4e0f
    Signed-off-by: Sebastian Grabowski <s.grabowski@samsung.com>

---
+++ src/tests/cynara-admin.cpp
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Contact: Rafal Krypa <r.krypa@samsung.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        cynara-admin.cpp
+ * @author      Sebastian Grabowski <s.grabowski@samsung.com>
+ * @brief       Implementation of unit tests for CynaraAdmin class
+ */
+/* vim: set ts=4 et sw=4 tw=78 : */
+
+#include "gtest/gtest.h"
+
+#define private public
+#include "cynara.h"
+#undef private
+
+using namespace SecurityManager;
+
+TEST(CynaraAdminTests, BucketManagement)
+{
+    const std::string b = "__wiadro_testowe_1_";
+    EXPECT_THROW(CynaraAdmin::getInstance().CreateBucket(b,
+                 CynaraAdminPolicy::Operation::Delete),
+                 CynaraException::InvalidParam);
+    EXPECT_THROW(CynaraAdmin::getInstance().CreateBucket(b,
+                 CynaraAdminPolicy::Operation::Bucket),
+                 CynaraException::InvalidParam);
+    EXPECT_NO_THROW({
+            CynaraAdmin::getInstance().RemoveBucket(b + 'a');
+            });
+    ASSERT_NO_THROW({
+            CynaraAdmin::getInstance().CreateBucket(b,
+                CynaraAdminPolicy::Operation::Deny);
+            });
+    EXPECT_NO_THROW({
+            CynaraAdmin::getInstance().CreateBucket(b,
+                CynaraAdminPolicy::Operation::Allow);
+            });
+    EXPECT_NO_THROW({
+            CynaraAdmin::getInstance().CreateBucket(b,
+                CynaraAdminPolicy::Operation::None);
+            });
+    EXPECT_NO_THROW({
+            CynaraAdmin::getInstance().RemoveBucket(b);
+            });
+}
+
