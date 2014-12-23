165b27d49e1c8f3533e2cbe62bbfbb77d4cf5a9c
Michal Eljasiewicz <m.eljasiewic@samsung.com>
2014-12-23 13:43:48 +0100

    Wrapper for cynara_admin_check.
    Asks Cynara for permission and allows to specify in
    which policy bucket to start search.
    
    Change-Id: I127057249437c8aa2c719e498239f8f0ef3f8ed9
    Signed-off-by: Michal Eljasiewicz <m.eljasiewic@samsung.com>

--- src/common/cynara.cpp
+++ src/common/cynara.cpp
@@ -111,7 +111,6 @@ CynaraAdmin::BucketsMap CynaraAdmin::Buckets =
     { Bucket::MANIFESTS, std::string("MANIFESTS")},
 };
 
-
 CynaraAdminPolicy::CynaraAdminPolicy(const std::string &client, const std::string &user,
         const std::string &privilege, int operation,
         const std::string &bucket)
@@ -443,6 +442,23 @@ int CynaraAdmin::convertToPolicyType(const std::string &policy, bool forceRefres
     FetchCynaraPolicyDescriptions(forceRefresh);
 
     return DescriptionToType.at(policy);
+}
+void CynaraAdmin::Check(const std::string &label, const std::string &privilege, const std::string &user,
+    const std::string &bucket, int &result, std::string &resultExtra, const bool recursive)
+{
+    char * cstr = nullptr;
+
+    checkCynaraError(
+        cynara_admin_check(m_CynaraAdmin, bucket.c_str(), recursive, label.c_str(),
+            user.c_str(), privilege.c_str(), &result, &cstr),
+        "Error while asking cynara admin API for permission for app label: " + label + ", user: "
+            + user + " privilege: " + privilege + " bucket: " + bucket + "\n");
+
+    if(cstr == nullptr)
+        return;
+
+    resultExtra = std::string(cstr);
+    free(cstr);
 }
 
 Cynara::Cynara()
