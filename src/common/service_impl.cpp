85a64f5e3c6b91c1a87dfedc76fd960e894e9b2e
Zbigniew Jasinski <z.jasinski@samsung.com>
2015-01-02 11:36:40 +0100

    New path types for application installation in security-manager
    security-manager now provides two path types:
    - writable, accessible for writing to the app and all apps within its package
    - readable, accessible to all apps for reading
    
    Test with https://review.tizen.org/gerrit/#/c/34509/
    
    Change-Id: I87a0dfbd4bc7b9e59d1ed97dc8332e7df20b139f

--- src/common/service_impl.cpp
+++ src/common/service_impl.cpp
@@ -109,12 +109,6 @@ static inline bool installRequestAuthCheck(const app_inst_req &req, uid_t uid)
             LogWarning("User's apps may have registered folders only in user's home dir");
             return false;
         }
-
-        app_install_path_type pathType = static_cast<app_install_path_type>(appPath.second);
-        if (pathType == SECURITY_MANAGER_PATH_PUBLIC) {
-            LogWarning("Only root can register SECURITY_MANAGER_PATH_PUBLIC path");
-            return false;
-        }
     }
     return true;
 }
@@ -202,7 +196,7 @@ int appInstall(const app_inst_req &req, uid_t uid)
     for (const auto &appPath : req.appPaths) {
         const std::string &path = appPath.first;
         app_install_path_type pathType = static_cast<app_install_path_type>(appPath.second);
-        int result = setupPath(req.pkgId, path, pathType);
+        int result = setupPath(req.appId, path, pathType);
 
         if (!result) {
             LogError("setupPath() failed");
