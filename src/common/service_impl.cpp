6ac8c4a2f98bf8c86388e55ed4791307a0d68e1a
Sebastian Grabowski <s.grabowski@samsung.com>
2015-01-14 11:46:42 +0100

    Added initial support for loading Tizen 3.0 privileges list
    New functions were added: loadPrivileges and getPrivileges in
    SecurityManager::ServiceImpl namespace.
    
    Change-Id: Ia37c9dde7939f09ca21569ddc33d7f1c176d8ea3
    Signed-off-by: Sebastian Grabowski <s.grabowski@samsung.com>

--- src/common/service_impl.cpp
+++ src/common/service_impl.cpp
@@ -30,9 +30,11 @@
 
 #include <cstring>
 #include <algorithm>
+#include <fstream>
 
 #include <dpl/log/log.h>
 #include <tzplatform_config.h>
+#include <boost/algorithm/string.hpp>
 
 #include "protocols.h"
 #include "privilege_db.h"
@@ -46,6 +48,11 @@
 
 namespace SecurityManager {
 namespace ServiceImpl {
+
+static const std::string privilegesListFileName = "privileges-tizen.list";
+static const std::string privilegesListFile = std::string(USERTYPE_POLICY_PATH) +
+                                              "/" + privilegesListFileName;
+static std::vector<std::string> allPrivileges;
 
 static uid_t getGlobalUserId(void)
 {
@@ -468,6 +475,41 @@ int reloadUserTypePolicy(uid_t uid)
         };
         closedir(dir);
     } else ret = SECURITY_MANAGER_API_ERROR_FILE_NOT_EXIST;
+
+    return ret;
+}
+
+int loadPrivileges(bool reload)
+{
+    if ((allPrivileges.size() == 0) || (reload)) {
+        LogDebug("Loading privileges file '" << privilegesListFileName << "'");
+        allPrivileges.clear();
+        std::ifstream fs(privilegesListFile);
+        if (!fs.is_open())
+            return SECURITY_MANAGER_API_ERROR_FILE_NOT_EXIST;
+        for (std::string line; std::getline(fs, line); ) {
+            boost::algorithm::trim(line);
+            if (line.empty() || (line[0] == '\''))
+                continue;
+            LogDebug(privilegesListFileName << ": " << line);
+            allPrivileges.push_back(line);
+        }
+    } else {
+        LogDebug("Privileges list is already loaded and not forced to reload.");
+    }
+    return allPrivileges.size() > 0 ?
+           SECURITY_MANAGER_API_SUCCESS :
+           SECURITY_MANAGER_API_ERROR_LOADING_PRIVILEGES_LIST;
+}
+
+int getAllSystemPrivileges(std::vector<std::string> &privilegesList)
+{
+    int ret = SECURITY_MANAGER_API_SUCCESS;
+
+    if (allPrivileges.size() == 0)
+        ret = loadPrivileges();
+    if (SECURITY_MANAGER_API_SUCCESS == ret)
+        privilegesList = allPrivileges;
 
     return ret;
 }
