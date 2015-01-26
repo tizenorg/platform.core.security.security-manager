a92d43160549da340fac3436284e522c5102294b
Michal Eljasiewicz <m.eljasiewic@samsung.com>
2015-01-26 12:27:03 +0100

    Wrapper for cynara_admin_list_policies_descriptions
    Change-Id: I6b07e4fb0b8e1395a3d867bcdecf1e79b3839772
    Signed-off-by: Michal Eljasiewicz <m.eljasiewic@samsung.com>

--- src/common/include/cynara.h
+++ src/common/include/cynara.h
@@ -91,6 +91,9 @@ public:
     typedef std::map<Bucket, const std::string > BucketsMap;
     static BucketsMap Buckets;
 
+    typedef  std::map<int, std::string> TypeToDescriptionsMap;
+    typedef  std::map<std::string, int> DescriptionsToTypeMap;
+
     virtual ~CynaraAdmin();
 
     static CynaraAdmin &getInstance();
@@ -159,7 +162,47 @@ public:
         const std::string &privilege,
         std::vector<CynaraAdminPolicy> &policies);
 
+    /**
+     * Wrapper for Cynara API function cynara_admin_list_policies_descriptions.
+     * It collects all policies descriptions, extracts names
+     * of policies and returns as std strings. Caller is responsible for clearing
+     * vector passed as argument.
+     *
+     * @param policiesDescriptions empty vector for policies descriptions.
+     */
+    void ListPoliciesDescriptions(std::vector<std::string> &policiesDescriptions);
+
+    /**
+     * Function translates internal Cynara policy type integer to string
+     * description. Descriptions are retrieved from Cynara using
+     * ListPoliciesDescriptions() function. Caller can force refetching of
+     * descriptions list from Cynara on each call.
+     *
+     * @throws std::out_of_range
+     *
+     * @param policyType Cynara policy result type.
+     * @param forceRefresh switch to force refetching of descriptions from Cynara.
+     */
+    std::string convertToPolicyDescription(const int policyType, bool forceRefresh = false);
+
+    /**
+     * Function translates Cynara policy result string
+     * description to internal Cynara policy type integer.
+     * Descriptions are retrieved from Cynara using
+     * ListPoliciesDescriptions() function. Caller can force refetching of
+     * descriptions list from Cynara on each call.
+     *
+     * @throws std::out_of_range
+     *
+     * @param policy Cynara policy result string description.
+     * @param forceRefresh switch to force refetching of descriptions from Cynara.
+     */
+    int convertToPolicyType(const std::string &policy, bool forceRefresh = false);
+
 private:
+    static TypeToDescriptionsMap TypeToDescriptionsMapping;
+    static DescriptionsToTypeMap DescriptionsToTypeMapping;
+
     CynaraAdmin();
 
     /**
@@ -174,7 +217,15 @@ private:
     void EmptyBucket(const std::string &bucketName, bool recursive,
         const std::string &client, const std::string &user, const std::string &privilege);
 
+    /**
+     * Get Cynara policies result descriptions and cache them in std::map
+     *
+     * @param forceRefresh true if you want to reinitialize mappings
+     */
+    void FetchCynaraPolicyDescriptions(bool forceRefresh = false);
+
     struct cynara_admin *m_CynaraAdmin;
+    bool m_policyDescriptionsInitialized;
 };
 
 class Cynara
