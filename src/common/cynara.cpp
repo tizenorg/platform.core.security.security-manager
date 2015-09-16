Samsung/security-manager

commit 626f947e0bb6fd90d4c20fd914981d5b752ab1e6

Change smack labeling to be appId based.
* Generation of rules for all applications in a package so
  that they can share resources

* Smack labels are now appId based (appId refers to app_name
  in the database)

Change-Id: I464ca4b1a4558a0579b9da69b5b599d07340a60d

src/common/cynara.cpp
@@ -161,7 +161,7 @@ void CynaraAdmin::SetPolicies(const std::vector<CynaraAdminPolicy> &policies)
         "Error while updating Cynara policy.");
 }
 
-void CynaraAdmin::UpdatePackagePolicy(
+void CynaraAdmin::UpdateAppPolicy(
     const std::string &label,
     const std::string &user,
     const std::vector<std::string> &oldPrivileges,

src/common/include/cynara.h
@@ -101,7 +101,7 @@ public:
      * TODO: drop oldPrivileges argument and get them directly from Cynara.
      * Appropriate Cynara interface is needed first.
      */
-    static void UpdatePackagePolicy(const std::string &label, const std::string &user,
+    static void UpdateAppPolicy(const std::string &label, const std::string &user,
         const std::vector<std::string> &oldPrivileges,
         const std::vector<std::string> &newPrivileges);
 

src/common/service_impl.cpp
@@ -36,7 +36,6 @@
 #include "protocols.h"
 #include "privilege_db.h"
 #include "cynara.h"
-#include "smack-common.h"
 #include "smack-rules.h"
 #include "smack-labels.h"
 
@@ -122,10 +121,9 @@ static inline bool installRequestAuthCheck(const app_inst_req &req, uid_t uid)
 
 int appInstall(const app_inst_req &req, uid_t uid)
 {
-    bool pkgIdIsNew = false;
     std::vector<std::string> addedPermissions;
     std::vector<std::string> removedPermissions;
-
+    std::vector<std::string> pkgContents;
     std::string uidstr;
     if (uid) {
         if (uid != req.uid) {
@@ -145,8 +143,8 @@ int appInstall(const app_inst_req &req, uid_t uid)
     }
 
     std::string smackLabel;
-    if (!generateAppLabel(req.pkgId, smackLabel)) {
-        LogError("Cannot generate Smack label for package: " << req.pkgId);
+    if (!generateAppLabel(req.appId, smackLabel)) {
+        LogError("Cannot generate Smack label for application: " << req.appId);
         return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
     }
 
@@ -162,13 +160,11 @@ int appInstall(const app_inst_req &req, uid_t uid)
     pp_permissions[req.privileges.size()] = nullptr;
 
     try {
-        std::vector<std::string> oldPkgPrivileges, newPkgPrivileges;
-
+        std::vector<std::string> oldAppPrivileges;
         LogDebug("Install parameters: appId: " << req.appId << ", pkgId: " << req.pkgId
                  << ", uidstr " << uidstr << ", generated smack label: " << smackLabel);
 
         PrivilegeDb::getInstance().BeginTransaction();
-
         std::string pkg;
         bool ret = PrivilegeDb::getInstance().GetAppPkgId(req.appId, pkg);
         if (ret == true && pkg != req.pkgId) {
@@ -176,12 +172,13 @@ int appInstall(const app_inst_req &req, uid_t uid)
             PrivilegeDb::getInstance().RollbackTransaction();
             return SECURITY_MANAGER_API_ERROR_INPUT_PARAM;
         }
-        PrivilegeDb::getInstance().GetPkgPrivileges(req.pkgId, uid, oldPkgPrivileges);
-        PrivilegeDb::getInstance().AddApplication(req.appId, req.pkgId, uid, pkgIdIsNew);
+        PrivilegeDb::getInstance().GetAppPrivileges(req.appId, uid, oldAppPrivileges);
+        PrivilegeDb::getInstance().AddApplication(req.appId, req.pkgId, uid);
         PrivilegeDb::getInstance().UpdateAppPrivileges(req.appId, uid, req.privileges);
-        PrivilegeDb::getInstance().GetPkgPrivileges(req.pkgId, uid, newPkgPrivileges);
-        CynaraAdmin::UpdatePackagePolicy(smackLabel, uidstr, oldPkgPrivileges,
-                                         newPkgPrivileges);
+        /* Get all application ids in the package to generate rules withing the package */
+        PrivilegeDb::getInstance().GetAppIdsForPkgId(req.pkgId, pkgContents);
+        CynaraAdmin::UpdateAppPolicy(smackLabel, uidstr, oldAppPrivileges,
+                                         req.privileges);
         PrivilegeDb::getInstance().CommitTransaction();
         LogDebug("Application installation commited to database");
     } catch (const PrivilegeDb::Exception::IOError &e) {
@@ -213,12 +210,11 @@ int appInstall(const app_inst_req &req, uid_t uid)
         }
     }
 
-    if (pkgIdIsNew) {
-        LogDebug("Adding Smack rules for new pkgId " << req.pkgId);
-        if (!SmackRules::installPackageRules(req.pkgId)) {
-            LogError("Failed to apply package-specific smack rules");
-            return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
-        }
+    LogDebug("Adding Smack rules for new appId: " << req.appId << " with pkgId: "
+            << req.pkgId << ". Applications in package: " << pkgContents.size());
+    if (!SmackRules::installApplicationRules(req.appId, req.pkgId, pkgContents)) {
+        LogError("Failed to apply package-specific smack rules");
+        return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
     }
 
     return SECURITY_MANAGER_API_SUCCESS;
@@ -228,13 +224,14 @@ int appUninstall(const std::string &appId, uid_t uid)
 {
     std::string pkgId;
     std::string smackLabel;
+    std::vector<std::string> pkgContents;
     bool appExists = true;
     bool removePkg = false;
     std::string uidstr;
     checkGlobalUser(uid, uidstr);
 
     try {
-        std::vector<std::string> oldPkgPrivileges, newPkgPrivileges;
+        std::vector<std::string> oldAppPrivileges;
 
         PrivilegeDb::getInstance().BeginTransaction();
         if (!PrivilegeDb::getInstance().GetAppPkgId(appId, pkgId)) {
@@ -247,17 +244,20 @@ int appUninstall(const std::string &appId, uid_t uid)
             LogDebug("Uninstall parameters: appId: " << appId << ", pkgId: " << pkgId
                      << ", uidstr " << uidstr << ", generated smack label: " << smackLabel);
 
-            if (!generateAppLabel(pkgId, smackLabel)) {
+            if (!generateAppLabel(appId, smackLabel)) {
                 LogError("Cannot generate Smack label for package: " << pkgId);
                 return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
             }
 
-            PrivilegeDb::getInstance().GetPkgPrivileges(pkgId, uid, oldPkgPrivileges);
+            /* Before we remove the app from the database, let's fetch all apps in the package
+                that this app belongs to, this will allow us to remove all rules withing the
+                package that the app appears in */
+            PrivilegeDb::getInstance().GetAppIdsForPkgId(pkgId, pkgContents);
+            PrivilegeDb::getInstance().GetAppPrivileges(appId, uid, oldAppPrivileges);
             PrivilegeDb::getInstance().UpdateAppPrivileges(appId, uid, std::vector<std::string>());
             PrivilegeDb::getInstance().RemoveApplication(appId, uid, removePkg);
-            PrivilegeDb::getInstance().GetPkgPrivileges(pkgId, uid, newPkgPrivileges);
-            CynaraAdmin::UpdatePackagePolicy(smackLabel, uidstr, oldPkgPrivileges,
-                                             newPkgPrivileges);
+            CynaraAdmin::UpdateAppPolicy(smackLabel, uidstr, oldAppPrivileges,
+                                             std::vector<std::string>());
             PrivilegeDb::getInstance().CommitTransaction();
             LogDebug("Application uninstallation commited to database");
         }
@@ -286,6 +286,11 @@ int appUninstall(const std::string &appId, uid_t uid)
                 LogError("Error on uninstallation of package-specific smack rules");
                 return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
             }
+        }
+        LogDebug ("Removing smack rules for deleted appId " << appId);
+        if (!SmackRules::uninstallApplicationRules(appId, pkgId, pkgContents)) {
+            LogError("Error during uninstallation of application-specific smack rules");
+            return SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
         }
     }
 
@@ -326,9 +331,8 @@ int getAppGroups(const std::string &appId, uid_t uid, pid_t pid, std::unordered_
             return SECURITY_MANAGER_API_ERROR_NO_SUCH_OBJECT;
         }
         LogDebug("pkgId: " << pkgId);
-
-        if (!generateAppLabel(pkgId, smackLabel)) {
-             LogError("Cannot generate Smack label for package: " << pkgId);
+        if (!generatePkgLabel(pkgId, smackLabel)) {
+            LogError("Cannot generate Smack label for pkgId: " << pkgId);
             return SECURITY_MANAGER_API_ERROR_NO_SUCH_OBJECT;
         }
         LogDebug("smack label: " << smackLabel);

src/client/client-common.cpp
@@ -38,7 +38,6 @@
 #include <dpl/singleton_safe_impl.h>
 
 #include <message-buffer.h>
-#include <smack-common.h>
 
 #include <protocols.h>
 

src/client/client-security-manager.cpp
@@ -39,11 +39,10 @@
 
 #include <dpl/log/log.h>
 #include <dpl/exception.h>
-
+#include <smack-labels.h>
 #include <message-buffer.h>
 #include <client-common.h>
 #include <protocols.h>
-#include <smack-common.h>
 #include <service_impl.h>
 #include <file-lock.h>
 
@@ -360,7 +359,6 @@ static bool setup_smack(const char *label)
 SECURITY_MANAGER_API
 int security_manager_set_process_label_from_appid(const char *app_id)
 {
-    char *pkg_id;
     int ret;
     std::string appLabel;
 
@@ -369,23 +367,17 @@ int security_manager_set_process_label_from_appid(const char *app_id)
     if (smack_smackfs_path() == NULL)
         return SECURITY_MANAGER_SUCCESS;
 
-    ret = security_manager_get_app_pkgid(&pkg_id, app_id);
-    if (ret != SECURITY_MANAGER_SUCCESS) {
+    if (SecurityManager::generateAppLabel(std::string(app_id), appLabel) == false) {
+        LogError("Failed to generate smack label for appId: " << app_id);
+        return SECURITY_MANAGER_API_ERROR_NO_SUCH_OBJECT;
+    }
+
+    if ((ret = setup_smack(appLabel.c_str())) != SECURITY_MANAGER_SUCCESS) {
+        LogError("Failed to set smack label " << appLabel << " for current process");
         return ret;
     }
 
-    if (SecurityManager::generateAppLabel(std::string(pkg_id), appLabel)) {
-        ret = setup_smack(appLabel.c_str());
-        if (ret != SECURITY_MANAGER_SUCCESS) {
-            LogError("Failed to set smack label " << appLabel << " for current process");
-        }
-    }
-    else {
-        ret = SECURITY_MANAGER_ERROR_UNKNOWN;
-    }
-
-    free(pkg_id);
-    return ret;
+    return SECURITY_MANAGER_SUCCESS;
 }
 
 SECURITY_MANAGER_API

src/common/CMakeLists.txt
@@ -46,7 +46,6 @@ SET(COMMON_SOURCES
     ${COMMON_PATH}/protocols.cpp
     ${COMMON_PATH}/message-buffer.cpp
     ${COMMON_PATH}/privilege_db.cpp
-    ${COMMON_PATH}/smack-common.cpp
     ${COMMON_PATH}/smack-labels.cpp
     ${COMMON_PATH}/smack-rules.cpp
     ${COMMON_PATH}/smack-check.cpp

src/common/include/privilege_db.h
@@ -45,6 +45,7 @@ const char *const PRIVILEGE_DB_PATH = tzplatform_mkpath(TZ_SYS_DB, ".security-ma
 
 enum class QueryType {
     EGetPkgPrivileges,
+    EGetAppPrivileges,
     EAddApplication,
     ERemoveApplication,
     EAddAppPrivileges,
@@ -53,6 +54,7 @@ enum class QueryType {
     EGetPkgId,
     EGetPrivilegeGroups,
     EGetUserApps,
+    EGetAppsInPkg
 };
 
 class PrivilegeDb {
@@ -71,6 +73,7 @@ private:
     SecurityManager::DB::SqlConnection *mSqlConnection;
     const std::map<QueryType, const char * const > Queries = {
         { QueryType::EGetPkgPrivileges, "SELECT DISTINCT privilege_name FROM app_privilege_view WHERE pkg_name=? AND uid=? ORDER BY privilege_name"},
+        { QueryType::EGetAppPrivileges, "SELECT DISTINCT privilege_name FROM app_privilege_view WHERE app_name=? AND uid=? ORDER BY privilege_name"},
         { QueryType::EAddApplication, "INSERT INTO app_pkg_view (app_name, pkg_name, uid) VALUES (?, ?, ?)" },
         { QueryType::ERemoveApplication, "DELETE FROM app_pkg_view WHERE app_name=? AND uid=?" },
         { QueryType::EAddAppPrivileges, "INSERT INTO app_privilege_view (app_name, uid, privilege_name) VALUES (?, ?, ?)" },
@@ -79,6 +82,7 @@ private:
         { QueryType::EGetPkgId, " SELECT pkg_name FROM app_pkg_view WHERE app_name = ?" },
         { QueryType::EGetPrivilegeGroups, " SELECT name FROM privilege_group_view WHERE privilege_name = ?" },
         { QueryType::EGetUserApps, "SELECT name FROM app WHERE uid=?" },
+        { QueryType::EGetAppsInPkg, " SELECT app_name FROM app_pkg_view WHERE pkg_name = ?" },
     };
 
     /**
@@ -170,16 +174,26 @@ public:
             std::vector<std::string> &currentPrivilege);
 
     /**
+     * Retrieve list of privileges assigned to an appId
+     *
+     * @param appId - application identifier
+     * @param uid - user identifier for whom privileges will be retrieved
+     * @param[out] currentPrivileges - list of current privileges assigned to appId
+     * @exception DB::SqlConnection::Exception::InternalError on internal error
+     */
+    void GetAppPrivileges(const std::string &appId, uid_t uid,
+        std::vector<std::string> &currentPrivileges);
+
+    /**
      * Add an application into the database
      *
      * @param appId - application identifier
      * @param pkgId - package identifier
      * @param uid - user identifier for whom application is going to be installed
-     * @param[out] pkgIdIsNew - return info if pkgId is new to the database
      * @exception DB::SqlConnection::Exception::InternalError on internal error
      */
     void AddApplication(const std::string &appId, const std::string &pkgId,
-            uid_t uid, bool &pkgIdIsNew);
+            uid_t uid);
 
     /**
      * Remove an application from the database
@@ -232,6 +246,15 @@ public:
      * @exception DB::SqlConnection::Exception::InternalError on internal error
      */
     void GetUserApps(uid_t uid, std::vector<std::string> &apps);
+    /**
+     * Retrieve a list of all application ids for a package id
+     *
+     * @param pkgId - package id
+     * @param[out] appIds - list of application ids for the package id
+     * @exception DB::SqlConnection::Exception::InternalError on internal error
+     */
+    void GetAppIdsForPkgId (const std::string &pkgId,
+        std::vector<std::string> &appIds);
 };
 
 } //namespace SecurityManager

src/common/include/smack-common.h
@@ -1,44 +1,0 @@
-/*
- *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
- *
- *  Contact: Rafal Krypa <r.krypa@samsung.com>
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- */
-/*
- * @file        smack-common.h
- * @author      Jacek Bukarewicz <j.bukarewicz@samsung.com>
- * @author      Jan Cybulski <j.cybulski@samsung.com>
- * @version     1.0
- * @brief       Header file for smack-related functions and constants
- */
-#ifndef _SMACK_COMMON_H_
-#define _SMACK_COMMON_H_
-
-#include <string>
-#include <linux/xattr.h>
-
-namespace SecurityManager {
-    /**
-     * Generates label for application with package identifier
-     * read from @ref pkgId and assigns it to @ref label.
-     *
-     * @param[in] pkgId application's package identifier
-     * @param[out] label string in which application's label will be stored
-     * @return true on success, false on error.
-    */
-    bool generateAppLabel(const std::string &pkgId, std::string &label);
-}
-
-#endif /* _SMACK_COMMON_H_ */
-

tizenorg/platform.core.security.security-manager

commit 57f545dfd9808a71273265c0b1db98f0125a0a3e

Rewrite and fix CynaraAdmin::SetPolicies
Method CynaraAdmin::Setpolicies, updating Cynara policy for an application,
was previously written to accept two vectors of privileges:
previously enabled privileges and privileges that should be enabled.
Vectors were used to calculate privileges to disable and privileges to
enable in Cynara. It required that both vectors are sorted and without
duplicates. Callers of this method fetched privileges from data base, which
provides sorting and unification.

This was broken in commit 626f947e0bb6fd90d4c20fd914981d5b752ab1e6
(Change smack labeling to be appId based). The second vector was taken
directly from application installation request, that wasn't necessarily
sorted or unique.

This method can be simplified now withot the need for sorted vectors. In
fact only one vector is necessarry now, because cynara-admin provides
support for listing policies (it didn't when the method was initially
written). Now it only takes vector of privileges that should be enabled,
in arbitrary order, that may contain duplicates. It lists previously enabled
privileges directly from Cynara, calculates the difference and sends
updated policies back to Cynara.

Change-Id: I15ca331cf5f46ae43c7665977df7eb4d3c7e986c
Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

src/common/cynara.cpp
@@ -22,6 +22,7 @@
  */
 
 #include <cstring>
+#include <unordered_set>
 #include "cynara.h"
 
 #include <dpl/log/log.h>
@@ -286,53 +287,35 @@ void CynaraAdmin::SetPolicies(const std::vector<CynaraAdminPolicy> &policies)
 void CynaraAdmin::UpdateAppPolicy(
     const std::string &label,
     const std::string &user,
-    const std::vector<std::string> &oldPrivileges,
-    const std::vector<std::string> &newPrivileges)
+    const std::vector<std::string> &privileges)
 {
+    std::unordered_set<std::string> privilegesSet(privileges.begin(), privileges.end());
+
     std::vector<CynaraAdminPolicy> policies;
+    CynaraAdmin::getInstance().ListPolicies(
+        CynaraAdmin::Buckets.at(Bucket::MANIFESTS),
+        CYNARA_ADMIN_ANY, CYNARA_ADMIN_ANY, CYNARA_ADMIN_ANY, policies);
 
-    // Perform sort-merge join on oldPrivileges and newPrivileges.
-    // Assume that they are already sorted and without duplicates.
-    auto oldIter = oldPrivileges.begin();
-    auto newIter = newPrivileges.begin();
-
-    while (oldIter != oldPrivileges.end() && newIter != newPrivileges.end()) {
-        int compare = oldIter->compare(*newIter);
-        if (compare == 0) {
+    // Compare previous policies with set of new requested privileges
+    for (auto &policy : policies) {
+        if (privilegesSet.erase(policy.privilege)) {
+            // privilege was found and removed from the set, keeping policy
             LogDebug("(user = " << user << " label = " << label << ") " <<
-                "keeping privilege " << *newIter);
-            ++oldIter;
-            ++newIter;
-            continue;
-        } else if (compare < 0) {
-            LogDebug("(user = " << user << " label = " << label << ") " <<
-                "removing privilege " << *oldIter);
-            policies.push_back(CynaraAdminPolicy(label, user, *oldIter,
-                    static_cast<int>(CynaraAdminPolicy::Operation::Delete),
-                    Buckets.at(Bucket::MANIFESTS)));
-            ++oldIter;
+                "keeping privilege " << policy.privilege);
         } else {
+            // privilege was not found in the set, deleting policy
+            policy.result = static_cast<int>(CynaraAdminPolicy::Operation::Delete);
             LogDebug("(user = " << user << " label = " << label << ") " <<
-                "adding privilege " << *newIter);
-            policies.push_back(CynaraAdminPolicy(label, user, *newIter,
-                    static_cast<int>(CynaraAdminPolicy::Operation::Allow),
-                    Buckets.at(Bucket::MANIFESTS)));
-            ++newIter;
+                "removing privilege " << policy.privilege);
         }
     }
 
-    for (; oldIter != oldPrivileges.end(); ++oldIter) {
+    // Add policies for privileges that weren't previously enabled
+    // Those that were previously enabled are now removed from privilegesSet
+    for (const auto privilege : privilegesSet) {
         LogDebug("(user = " << user << " label = " << label << ") " <<
-            "removing privilege " << *oldIter);
-        policies.push_back(CynaraAdminPolicy(label, user, *oldIter,
-                    static_cast<int>(CynaraAdminPolicy::Operation::Delete),
-                    Buckets.at(Bucket::MANIFESTS)));
-    }
-
-    for (; newIter != newPrivileges.end(); ++newIter) {
-        LogDebug("(user = " << user << " label = " << label << ") " <<
-            "adding privilege " << *newIter);
-        policies.push_back(CynaraAdminPolicy(label, user, *newIter,
+            "adding privilege " << privilege);
+        policies.push_back(CynaraAdminPolicy(label, user, privilege,
                     static_cast<int>(CynaraAdminPolicy::Operation::Allow),
                     Buckets.at(Bucket::MANIFESTS)));
     }

src/common/include/cynara.h
@@ -118,26 +118,18 @@ public:
     void SetPolicies(const std::vector<CynaraAdminPolicy> &policies);
 
     /**
-     * Update Cynara policies for the package and the user, using two vectors
-     * of privileges: privileges set before (and already enabled in Cynara)
-     * and new privileges, to be set in Cynara.
+     * Update Cynara policies for the application and the user.
      * Difference will be calculated, removing old unneeded privileges and
      * adding new, previously not enabled privileges.
      * Caller must have permission to access Cynara administrative socket.
      *
      * @param label application Smack label
      * @param user user identifier
-     * @param oldPrivileges previously enabled privileges for the package.
-     *        Must be sorted and without duplicates.
-     * @param newPrivileges currently enabled privileges for the package.
-     *        Must be sorted and without duplicates.
+     * @param privileges currently enabled privileges
      *
-     * TODO: drop oldPrivileges argument and get them directly from Cynara.
-     * Appropriate Cynara interface is needed first.
      */
     void UpdateAppPolicy(const std::string &label, const std::string &user,
-        const std::vector<std::string> &oldPrivileges,
-        const std::vector<std::string> &newPrivileges);
+        const std::vector<std::string> &privileges);
 
     /**
      * Depending on user type, create link between MAIN bucket and appropriate

src/common/include/master-req.h
@@ -39,16 +39,12 @@ namespace MasterReq {
  *
  * @param[in]  appID            Application ID
  * @param[in]  uidstr           String containing user identifier
- * @param[in]  oldPkgPrivileges Previously enabled privileges for the package,
- *                              Must be sorted and without duplicates
- * @param[in]  newPkgPrivileges Currently enabled privileges for the package,
- *                              Must be sorted and without duplicates
+ * @param[in]  privileges       Currently enabled privileges for the application
  *
  * @see CynaraAdmin::UpdateAppPolicy
  */
 int CynaraPolicyUpdate(const std::string &appId, const std::string &uidstr,
-                       const std::vector<std::string> &oldPkgPrivileges,
-                       const std::vector<std::string> &newPkgPrivileges);
+                       const std::vector<std::string> &privileges);
 
 /**
  * Forwards Cynara user initialization to Master service.

src/common/master-req.cpp
@@ -32,16 +32,15 @@
 namespace SecurityManager {
 namespace MasterReq {
 
-int CynaraPolicyUpdate(const std::string &appId,  const std::string &uidstr,
-                       const std::vector<std::string> &oldPkgPrivileges,
-                       const std::vector<std::string> &newPkgPrivileges)
+int CynaraPolicyUpdate(const std::string &appId, const std::string &uidstr,
+                       const std::vector<std::string> &privileges)
 {
     int ret;
     MessageBuffer sendBuf, retBuf;
 
     Serialization::Serialize(sendBuf,
         static_cast<int>(MasterSecurityModuleCall::CYNARA_UPDATE_POLICY),
-        appId, uidstr, oldPkgPrivileges, newPkgPrivileges);
+        appId, uidstr, privileges);
 
     ret = sendToServer(MASTER_SERVICE_SOCKET, sendBuf.Pop(), retBuf);
     if (ret == SECURITY_MANAGER_API_SUCCESS)

src/common/service_impl.cpp
@@ -292,8 +292,6 @@ int ServiceImpl::appInstall(const app_inst_req &req, uid_t uid, bool isSlave)
     }
 
     try {
-        std::vector<std::string> oldAppPrivileges;
-
         appLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(req.appId), zoneId);
         /* NOTE: we don't use pkgLabel here, but generate it for pkgId validation */
         pkgLabel = zoneSmackLabelGenerate(SmackLabels::generatePkgLabel(req.pkgId), zoneId);
@@ -309,23 +307,21 @@ int ServiceImpl::appInstall(const app_inst_req &req, uid_t uid, bool isSlave)
             PrivilegeDb::getInstance().RollbackTransaction();
             return SECURITY_MANAGER_API_ERROR_INPUT_PARAM;
         }
-        PrivilegeDb::getInstance().GetAppPrivileges(req.appId, uid, oldAppPrivileges);
+
         PrivilegeDb::getInstance().AddApplication(req.appId, req.pkgId, uid);
         PrivilegeDb::getInstance().UpdateAppPrivileges(req.appId, uid, req.privileges);
         /* Get all application ids in the package to generate rules withing the package */
         PrivilegeDb::getInstance().GetAppIdsForPkgId(req.pkgId, pkgContents);
 
         if (isSlave) {
-            int ret = MasterReq::CynaraPolicyUpdate(req.appId, uidstr, oldAppPrivileges,
-                                                    req.privileges);
+            int ret = MasterReq::CynaraPolicyUpdate(req.appId, uidstr, req.privileges);
             if (ret != SECURITY_MANAGER_API_SUCCESS) {
                 PrivilegeDb::getInstance().RollbackTransaction();
                 LogError("Error while processing request on master: " << ret);
                 return ret;
             }
         } else {
-            CynaraAdmin::getInstance().UpdateAppPolicy(appLabel, uidstr, oldAppPrivileges,
-                                                       req.privileges);
+            CynaraAdmin::getInstance().UpdateAppPolicy(appLabel, uidstr, req.privileges);
         }
 
         PrivilegeDb::getInstance().CommitTransaction();
@@ -408,8 +404,6 @@ int ServiceImpl::appUninstall(const std::string &appId, uid_t uid, bool isSlave)
     }
 
     try {
-        std::vector<std::string> oldAppPrivileges;
-
         PrivilegeDb::getInstance().BeginTransaction();
         if (!PrivilegeDb::getInstance().GetAppPkgId(appId, pkgId)) {
             LogWarning("Application " << appId <<
@@ -425,21 +419,18 @@ int ServiceImpl::appUninstall(const std::string &appId, uid_t uid, bool isSlave)
                 that this app belongs to, this will allow us to remove all rules withing the
                 package that the app appears in */
             PrivilegeDb::getInstance().GetAppIdsForPkgId(pkgId, pkgContents);
-            PrivilegeDb::getInstance().GetAppPrivileges(appId, uid, oldAppPrivileges);
             PrivilegeDb::getInstance().UpdateAppPrivileges(appId, uid, std::vector<std::string>());
             PrivilegeDb::getInstance().RemoveApplication(appId, uid, removePkg);
 
             if (isSlave) {
-                int ret = MasterReq::CynaraPolicyUpdate(appId, uidstr, oldAppPrivileges,
-                                                        std::vector<std::string>());
+                int ret = MasterReq::CynaraPolicyUpdate(appId, uidstr, std::vector<std::string>());
                 if (ret != SECURITY_MANAGER_API_SUCCESS) {
                     PrivilegeDb::getInstance().RollbackTransaction();
                     LogError("Error while processing request on master: " << ret);
                     return ret;
                 }
             } else {
-                CynaraAdmin::getInstance().UpdateAppPolicy(smackLabel, uidstr, oldAppPrivileges,
-                                                           std::vector<std::string>());
+                CynaraAdmin::getInstance().UpdateAppPolicy(smackLabel, uidstr, std::vector<std::string>());
             }
 
             PrivilegeDb::getInstance().CommitTransaction();
@@ -883,6 +874,7 @@ int ServiceImpl::getPolicy(const policy_entry &filter, uid_t uid, pid_t pid, con
                 std::vector<std::string> listOfPrivileges;
 
                 // FIXME: also fetch privileges of global applications
+                // FIXME: fetch privileges from cynara, drop PrivilegeDb::GetAppPrivileges
                 PrivilegeDb::getInstance().GetAppPrivileges(appId, uid, listOfPrivileges);
 
                 if (filter.privilege.compare(SECURITY_MANAGER_ANY)) {

src/server/service/master-service.cpp
@@ -170,19 +170,17 @@ void MasterService::processCynaraUpdatePolicy(MessageBuffer &buffer, MessageBuff
     int ret = SECURITY_MANAGER_API_ERROR_SERVER_ERROR;
     std::string appId;
     std::string uidstr;
-    std::vector<std::string> oldAppPrivileges, newAppPrivileges;
-    std::string appLabel, newLabel;
+    std::string appLabel;
+    std::vector<std::string> privileges;
 
     Deserialization::Deserialize(buffer, appId);
     Deserialization::Deserialize(buffer, uidstr);
-    Deserialization::Deserialize(buffer, oldAppPrivileges);
-    Deserialization::Deserialize(buffer, newAppPrivileges);
+    Deserialization::Deserialize(buffer, privileges);
 
     appLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(appId), zoneId);
 
     try {
-        CynaraAdmin::getInstance().UpdateAppPolicy(appLabel, uidstr, oldAppPrivileges,
-                                                   newAppPrivileges);
+        CynaraAdmin::getInstance().UpdateAppPolicy(appLabel, uidstr, privileges);
     } catch (const CynaraException::Base &e) {
         LogError("Error while setting Cynara rules for application: " << e.DumpToString());
         goto out;