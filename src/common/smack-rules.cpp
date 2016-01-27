0e457b876f8e0c5b116df221a0308dccf3d3f12b
Zofia Abramowska <z.abramowska@samsung.com>
2016-01-27 19:47:07 +0100

    Support rules for apply/drop sharing in SmackRules
    Change-Id: I25c25853dd8af6c77b554505fc9f5d0231fea389

--- src/common/smack-rules.cpp
+++ src/common/smack-rules.cpp
@@ -46,6 +46,14 @@ const char *const SMACK_APP_LABEL_TEMPLATE     = "~APP~";
 const char *const SMACK_PKG_LABEL_TEMPLATE     = "~PKG~";
 const char *const APP_RULES_TEMPLATE_FILE_PATH = tzplatform_mkpath4(TZ_SYS_SHARE, "security-manager", "policy", "app-rules-template.smack");
 const char *const SMACK_APP_IN_PACKAGE_PERMS   = "rwxat";
+const char *const SMACK_APP_PATH_OWNER_PERMS = "rwxat";
+const char *const SMACK_APP_PATH_TARGET_PERMS = "rxl";
+const char *const SMACK_APP_DIR_TARGET_PERMS = "x";
+const char *const SMACK_USER = "User";
+const char *const SMACK_SYSTEM = "System";
+const char *const SMACK_APP_PATH_SYSTEM_PERMS = "rwxat";
+const char *const SMACK_APP_PATH_USER_PERMS = "rwxat";
+
 
 SmackRules::SmackRules()
 {
@@ -306,6 +314,59 @@ void SmackRules::strReplace(std::string &haystack, const std::string &needle,
     size_t pos;
     while ((pos = haystack.find(needle)) != std::string::npos)
         haystack.replace(pos, needle.size(), replace);
+}
+
+void SmackRules::applyPrivateSharingRules(const std::string &ownerPkgId,
+                                          const std::vector<std::string> &ownerPkgContents,
+                                          const std::string &targetAppId,
+                                          const std::string &pathLabel,
+                                          bool isPathSharedAlready,
+                                          bool isTargetSharingAlready,
+                                          const std::string &zoneId)
+{
+    SmackRules rules;
+    const std::string &targetLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(targetAppId), zoneId);
+    if (!isTargetSharingAlready) {
+
+        rules.add(targetLabel,
+                  zoneSmackLabelGenerate(SmackLabels::generateAppLabel(ownerPkgId), zoneId),
+                  SMACK_APP_DIR_TARGET_PERMS);
+    }
+    if (!isPathSharedAlready) {
+        for (const auto &app: ownerPkgContents) {
+            const std::string appLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(app), zoneId);
+            rules.add(appLabel, pathLabel, SMACK_APP_PATH_OWNER_PERMS);
+        }
+        rules.add(SMACK_USER, pathLabel, SMACK_APP_PATH_USER_PERMS);
+        rules.add(SMACK_SYSTEM, pathLabel, SMACK_APP_PATH_SYSTEM_PERMS);
+    }
+    rules.add(targetLabel, pathLabel, SMACK_APP_PATH_TARGET_PERMS);
+}
+
+void SmackRules::dropPrivateSharingRules(const std::string &ownerPkgId,
+                                         const std::vector<std::string> &ownerPkgContents,
+                                         const std::string &targetAppId,
+                                         const std::string &pathLabel,
+                                         bool isPathSharedNoMore,
+                                         bool isTargetSharingNoMore,
+                                         const std::string &zoneId)
+{
+    SmackRules rules;
+    const std::string &targetLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(targetAppId), zoneId);
+    if (isTargetSharingNoMore) {
+        rules.addModify(targetLabel,
+                  zoneSmackLabelGenerate(SmackLabels::generateAppLabel(ownerPkgId), zoneId),
+                  "", SMACK_APP_DIR_TARGET_PERMS);
+    }
+    if (isPathSharedNoMore) {
+        for (const auto &app: ownerPkgContents) {
+            const std::string appLabel = zoneSmackLabelGenerate(SmackLabels::generateAppLabel(app), zoneId);
+            rules.addModify(appLabel, pathLabel, "", SMACK_APP_PATH_OWNER_PERMS);
+        }
+        rules.addModify(SMACK_USER, pathLabel, "", SMACK_APP_PATH_USER_PERMS);
+        rules.addModify(SMACK_SYSTEM, pathLabel, "", SMACK_APP_PATH_SYSTEM_PERMS);
+    }
+    rules.addModify(targetLabel, pathLabel, "", SMACK_APP_PATH_TARGET_PERMS);
 }
 
 } // namespace SecurityManager
