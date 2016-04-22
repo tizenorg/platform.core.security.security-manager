34ca8ed39eb0c3ff2225db8336ce519c0384f7e7
Rafal Krypa <r.krypa@samsung.com>
2016-04-22 16:14:31 +0200

    uique_ptr
    Change-Id: I6ddc85dee5c459809b7afaa67a588d8b78bb89ef

--- src/client/client-security-manager.cpp
+++ src/client/client-security-manager.cpp
@@ -48,6 +48,7 @@
 #include <service_impl.h>
 #include <connection.h>
 #include <credentials.h>
+#include <utils.h>
 
 #include <security-manager.h>
 #include <client-offline.h>
@@ -325,9 +326,8 @@ static bool setup_smack(const char *label)
 
     // Set Smack label for open socket file descriptors
 
-    std::unique_ptr<DIR, std::function<int(DIR*)>> dir(
-        opendir("/proc/self/fd"), closedir);
-    if (!dir.get()) {
+    auto dir = make_unique(opendir("/proc/self/fd"), closedir);
+    if (!dir) {
         LogError("Unable to read list of open file descriptors: " <<
             GetErrnoString(errno));
         return SECURITY_MANAGER_ERROR_UNKNOWN;
@@ -456,7 +456,7 @@ int security_manager_set_process_groups_from_appid(const char *app_name)
         oldGroupsCnt = ret;
 
         //Allocate an array for both old and new groups gids
-        std::unique_ptr<gid_t[]> groups(new gid_t[oldGroupsCnt + newGroupsCnt]);
+        auto groups = make_unique<gid_t[]>(oldGroupsCnt + newGroupsCnt);
         if (!groups.get()) {
             LogError("Memory allocation failed.");
             return SECURITY_MANAGER_ERROR_MEMORY;
@@ -985,11 +985,12 @@ int security_manager_groups_get(char ***groups, size_t *groups_count)
         const auto vgroups_size = vgroups.size();
         LogInfo("Number of groups: " << vgroups_size);
 
-        std::unique_ptr<char *, std::function<void(char **)>> array(
-            static_cast<char **>(calloc(vgroups_size, sizeof(char *))),
-            std::bind(security_manager_groups_free, std::placeholders::_1, vgroups_size));
+        auto array = make_unique(static_cast<char **>(calloc(vgroups_size, sizeof(char *))),
+            [vgroups_size](char **arr) {
+                return security_manager_groups_free(arr, vgroups_size);
+            });
 
-        if (array == nullptr)
+        if (!array)
             return SECURITY_MANAGER_ERROR_MEMORY;
 
         for (size_t i = 0; i < vgroups_size; ++i) {
