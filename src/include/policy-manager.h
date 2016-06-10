dc273cf2eb93af68ae56096e8d01c96cf8114996
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2016-06-10 13:42:52 +0200

    Fix doxygen comments
    This fixes all of the doxygen comment tags (/**) and a plethora of other
    errors and inconsistencies. Mostly missing argument names for \param.
    
    This is not a comprehensive doxygen comments review, but it does fix all
    the doxygen errors and warnings and makes it possible to generate
    somewhat correct doxygen documentation.
    
    Change-Id: Ib030dab7a5c116a8a6a9ccb3665dd79163a7b632

--- src/include/policy-manager.h
+++ src/include/policy-manager.h
@@ -414,9 +414,9 @@ void security_manager_policy_entries_free(policy_entry *p_entries, const size_t 
  * Caller needs to free memory allocated for the list using
  * security_manager_policy_levels_free().
  *
- * @param levels pointer to array of strings.
- * @param levels_count number of strings in levels array.
- * @return API return code or error code.
+ * \param levels        Pointer to array of strings
+ * \param levels_count  Number of strings in levels array
+ * \return API return code or error code.
  */
 int security_manager_policy_levels_get(char ***levels, size_t *levels_count);
 
@@ -424,9 +424,10 @@ int security_manager_policy_levels_get(char ***levels, size_t *levels_count);
  * This function free memory allocated by security_manager_policy_levels_get()
  * function.
  *
- * @param levels array of strings returned by
- * security_manager_policy_levels_get() function.
- * @return API return code or error code.
+ * \param levels        Array of strings returned by
+ *                      security_manager_policy_levels_get() function.
+ * \param levels_count  Number of strings in levels array
+ * \return API return code or error code.
  */
 void security_manager_policy_levels_free(char **levels, size_t levels_count);
 
