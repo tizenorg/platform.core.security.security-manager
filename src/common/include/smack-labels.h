64d3f8d47ca6d100ad08d3c9d9ea134d25a06a71
Bartlomiej Grzelewski <b.grzelewski@samsung.com>
2016-06-13 12:05:46 +0200

    Modify SmackLabels module
    Added:
     * getSmackLabelFromFd - extracts smack label from file descriptor
     * setSmackLabelForFd - set smack label for file connected with fd
    Modify:
     * pathSetSmack - use libsmack instead lsetxattr
    
    Change-Id: Ia5ceda42afc98dde0c8b7db2c0d0a0827efc4fa2

--- src/common/include/smack-labels.h
+++ src/common/include/smack-labels.h
@@ -147,12 +147,28 @@ std::string getSmackLabelFromPid(pid_t pid);
 std::string getSmackLabelFromPath(const std::string &path);
 
 /**
+ * Returns smack label for given file descriptor
+ *
+ * @param[in] fd file descriptor
+ * @return resulting Smack label
+ */
+std::string getSmackLabelFromFd(int fd);
+
+/**
  * Returns smack label for current process
  *
  * @param[in] sock socket file descriptor
  * @return resulting Smack label
  */
 std::string getSmackLabelFromSelf(void);
+
+/**
+ * Set up smack label for given file descriptor
+ *
+ * @param[in] fd file descriptor
+ * @return 0 on success. -1 on error
+ */
+void setSmackLabelForFd(int fd, const std::string &label);
 
 } // namespace SmackLabels
 } // namespace SecurityManager
